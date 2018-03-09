10896b4157122007b037fb7ae66d745a57735731
Alessandro Corrado <ale.corrado91@gmail.com>
2018-03-09 11:38:23 +0100

    Passaggio del localStorage al post

--- post.js
+++ post.js
@@ -1,23 +1,24 @@
-window.addEventListener('load',function(){
+window.addEventListener('load', function () {
     pageLoad();
-})
+});
 
-  function pageLoad(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(function(data){
-        console.log(data);
-        for(let i = 0; i< data.length; i++){
-            const cont = document.getElementById('contenitore');
-            const nome = document.createElement('div'); 
-            const corpo =document.createElement('div'); 
-            const post = data[i];
+function pageLoad() {
+    fetch('https://jsonplaceholder.typicode.com/posts/' + localStorage.idPost)
+        .then(response => response.json())
+        .then(function (data) {
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                const cont = document.getElementById('contenitore');
+                const nome = document.createElement('div');
+                const corpo = document.createElement('div');
+                const post = data[i];
 
-            nome.innerHTML = post.title;
-            corpo.innerHTML = post.body; 
-            
-            cont.appendChild(nome);
-            cont.appendChild(corpo);
-        }
-    })
- }
+                nome.innerHTML = post.title;
+                corpo.innerHTML = post.body;
+
+                cont.appendChild(nome);
+                cont.appendChild(corpo);
+            }
+        });
+}
+
